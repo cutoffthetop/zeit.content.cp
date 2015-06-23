c99791d5feec666dc5c522c53aca601b4c8a7690
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-06-23 13:16:15 +0200

    Back to development: 2.10.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='2.10.6',
+    version='2.10.7.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
