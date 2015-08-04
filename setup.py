48376b0509af1c1e9ca9e8d1292129f5ea310a3a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-08-04 12:39:51 +0200

    Preparing release 2.10.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='2.10.9.dev0',
+    version='2.10.9',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
