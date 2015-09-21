8cc4cb6ac817d35200e02304a4ee9e0f9c98a5b8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-09-21 13:39:15 +0200

    Backport ``IStoryStream`` interface so deserializing provided interfaces doesn't break.

--- src/zeit/content/cp/interfaces.py
+++ src/zeit/content/cp/interfaces.py
@@ -121,6 +121,10 @@ class ICP2015(ICenterPage):
     """Marker interfaces for CPs edited by the current CP-Editor (master)."""
 
 
+class IStoryStream(ICP2015):
+    """CP with a specific ``type``."""
+
+
 class CenterPageSource(zeit.cms.content.contentsource.CMSContentSource):
 
     name = 'zeit.content.cp'
